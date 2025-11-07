409111c38e03211a578e31b9cca774344c6bc716
andyhartzler <70266044+andyhartzler@users.noreply.github.com>
2025-11-07 12:12:04 -0600

    Restore IO ObjectBox exports for code generation

--- lib/database/models.dart
+++ lib/database/models.dart
@@ -3,11 +3,11 @@ export 'package:bluebubbles/database/io/attachment.dart'
 export 'package:bluebubbles/database/io/chat.dart'
     if (dart.library.html) 'package:bluebubbles/database/html/chat.dart';
 export 'package:bluebubbles/database/io/contact.dart'
-  if (dart.library.html) 'package:bluebubbles/database/html/contact.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/contact.dart';
 export 'package:media_kit/media_kit.dart'
-  if (dart.library.html) 'package:bluebubbles/database/html/media_kit.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/media_kit.dart';
 export 'package:media_kit_video/media_kit_video.dart'
-  if (dart.library.html) 'package:bluebubbles/database/html/media_kit.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/media_kit.dart';
 export 'package:bluebubbles/database/io/fcm_data.dart'
     if (dart.library.html) 'package:bluebubbles/database/html/fcm_data.dart';
 export 'package:bluebubbles/database/io/handle.dart'
@@ -16,14 +16,14 @@ export 'package:bluebubbles/database/io/launch_at_startup.dart'
     if (dart.library.html) 'package:bluebubbles/database/html/launch_at_startup.dart';
 export 'package:bluebubbles/database/io/message.dart'
     if (dart.library.html) 'package:bluebubbles/database/html/message.dart';
-export 'package:bluebubbles/database/html/objectbox.dart' hide Version
+export 'package:bluebubbles/database/html/objectbox.dart'
     if (dart.library.io) 'package:bluebubbles/objectbox.g.dart';
 export 'package:bluebubbles/database/io/theme.dart'
     if (dart.library.html) 'package:bluebubbles/database/html/theme.dart';
 export 'package:bluebubbles/database/io/theme_entry.dart'
-  if (dart.library.html) 'package:bluebubbles/database/html/theme_entry.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/theme_entry.dart';
 export 'package:bluebubbles/database/io/theme_object.dart'
-  if (dart.library.html) 'package:bluebubbles/database/html/theme_object.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/theme_object.dart';
 export 'package:bluebubbles/database/io/giphy.dart'
     if (dart.library.html) 'package:bluebubbles/database/html/giphy.dart';
 export 'package:bluebubbles/database/global/platform_file.dart';
