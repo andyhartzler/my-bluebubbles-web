ce66339dcf17b1ead112f607256d83dced203c88
andyhartzler <70266044+andyhartzler@users.noreply.github.com>
2025-11-07 13:56:39 -0600

    Fix LogicalKeySet const usage in CRM search dialog

--- lib/database/io/contact.dart
+++ lib/database/io/contact.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 
 import 'package:bluebubbles/database/database.dart';
 import 'package:bluebubbles/helpers/helpers.dart';
-import 'package:bluebubbles/database/models.dart';
+import 'package:bluebubbles/database/global/structured_name.dart';
+import 'package:bluebubbles/objectbox.g.dart'
+    if (dart.library.html) 'package:bluebubbles/database/html/objectbox.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
